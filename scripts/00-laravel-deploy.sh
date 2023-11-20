46dffccbd6e2c233e61855db5e6fa918b70e9128
Tenzin Tshomo <97825579+tnz99@users.noreply.github.com>
2023-11-21 04:07:00 +0600

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,8 +3,8 @@ echo "Running composer"
 # composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "Generate Applicaiton Key..."
-php artisan key:generate
+# echo "Generate Applicaiton Key..."
+# php artisan key:generate
 
 echo "Caching config..."
 php artisan config:cache
@@ -12,11 +12,11 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Installing migration.."
-php artisan migrate:install
+# echo "Installing migration.."
+# php artisan migrate:install
 
-echo "Running migrations..."
-php artisan migrate --force
+# echo "Running migrations..."
+# php artisan migrate --force
 
-echo "Seeding Admins..."
-php artisan db:seed
+# echo "Seeding Admins..."
+# php artisan db:seed
