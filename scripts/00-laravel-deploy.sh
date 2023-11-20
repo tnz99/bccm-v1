5f4995246c69a3f96a7529ecfbf1c0d9ad701c3d
Tenzin Tshomo <12200090.gcit@rub.edu.bt>
2023-11-21 03:11:25 +0600

    host

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+echo "Running composer"
+# composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Installing migration.."
+php artisan migrate:install
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Seeding Admins..."
+php artisan db:seed
