d47e94d32ecd5aed5fb8280a34dcd0913b4034a7
tnz99 <12200090.gcit@rub.edu.bt>
2023-11-21 18:58:19 +0600

    fix: get text/css not found
    Signed-off-by: tnz99 <12200090.gcit@rub.edu.bt>

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>BCCM | Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
 
     <link rel="stylesheet" type="text/css" href="/css/main.css">
     <link rel="stylesheet" type="text/css" href="/css/circular-nav.css">
     <link rel="stylesheet" type="text/css" href="/css/audio.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
-    <link rel="stylesheet" href="text/css" href="/css/prayerwheel.css" />
+    <link rel="stylesheet" type="text/css" href="/css/prayerwheel.css" />
     <link rel="stylesheet" type="text/css" href="/css/image-gallery.css">
     <link rel="stylesheet" type="text/css" href="/css/mobile.css">
 
